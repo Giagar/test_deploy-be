5e66b60547389c330257c29b29b0ddc6d5775175
Gianpaolo Garbarino <garbarino@digiunit.it>
2023-10-05 14:09:29 +0200

    Test: set home url

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 /* const mongoose = require('mongoose');
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@cluster0.n7tthgp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority` */
 
-app.get('/test', (req, res, next) => {
+app.get('/', (req, res, next) => {
     let posts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map(el => ({id: el, title: 'item' + el, content: 'content' + el}))
 
     res.status(200).json({ message: 'test page', data: { posts }, metadata: { status: 'success' }})
